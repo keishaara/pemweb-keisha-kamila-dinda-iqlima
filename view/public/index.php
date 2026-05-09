6592a38a4629b5837056f786be4d1a6ad72bafb5
keishaara <aurelkeisha331@gmail.com>
2026-05-09 20:47:58 +0700

    apdet ke php

--- view/public/index.php
+++ view/public/index.php
@@ -1,33 +1,37 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Evently - Platform Kegiatan Kampus #1</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <body>
     <div class="landing-page">
         <nav class="navbar">
-            <div class="logo"><img src="assets/img/icon.png" alt="Evently"> Evently</div>
+            <div class="logo"><img src="../../assets/img/icon.png" alt="Evently"> Evently</div>
             <div class="nav-links">
                 <a href="#">Fitur</a>
-                <a href="kegiatan.html">Kegiatan</a>
+                <a href="kegiatan.php">Kegiatan</a>
                 <a href="#">Tentang</a>
-                <a href="login.html" class="btn btn-outline btn-small active">Masuk</a>
-                <a href="register.html" class="btn btn-primary btn-small active">Daftar Gratis</a>
+                <?php if (!isset($_SESSION['user_id'])): ?>        
+                <?php else: ?>
+                    <a href="<?= ($_SESSION['role'] === 'admin') ? '../admin/dashboard.php' : '../mahasiswa/profil.php'; ?>" class="btn btn-outline btn-small active">Dashboard</a>
+                <?php endif; ?>
             </div>
         </nav>
 
         <section class="hero">
-            <div class="hero-badge">
-               Platform Kegiatan Kampus #1
-            </div>
+            <div class="hero-badge">Platform Kegiatan Kampus #1</div>
             <h1>Semua Kegiatan<br>Kampus, Satu <span>Tempat</span></h1>
             <p>Evently menyatukan informasi seminar, workshop, dan event organisasi kampus dalam satu platform terpusat yang mudah diakses.</p>
             <div class="hero-buttons">
-                <a href="register.html" class="btn btn-primary">Mulai Sekarang</a>
-                <a href="kegiatan.html" class="btn btn-outline">Lihat Kegiatan</a>
+                <a href="../auth/register.php" class="btn btn-primary">Mulai Sekarang</a>
+                <a href="kegiatan.php" class="btn btn-outline">Lihat Kegiatan</a>
             </div>
 
             <div class="stats">
