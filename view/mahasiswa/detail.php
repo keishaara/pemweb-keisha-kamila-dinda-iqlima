f5d79873fab49d31259f94c6d7f9b7636da5a2d3
keishaara <aurelkeisha331@gmail.com>
2026-05-28 09:14:15 +0700

    Update dashboard link to user_dashboard.php

--- view/mahasiswa/detail.php
+++ view/mahasiswa/detail.php
@@ -48,7 +48,7 @@ if(isset($_POST['simpan_event'])){
     <aside class="sidebar">
         <div class="logo"><img src="../../assets/img/icon.png" alt="Evently"> Evently</div>
         <div class="menu-category">Menu</div>
-        <a href="dashboard_mhs.php" class="menu-item"><img src="../../assets/img/icon-home2.png" alt="Home"> Beranda</a>
+        <a href="user_dashboard.php" class="menu-item"><img src="../../assets/img/icon-home2.png" alt="Home"> Beranda</a>
         <a href="kegiatan_mhs.php" class="menu-item active"><img src="../../assets/img/icon-kegiatan.png" alt="Kegiatan"> Kegiatan</a> 
         <a href="e-tiket.php" class="menu-item"><img src="../../assets/img/icon-ticket.png" alt="E-Tiket"> E-Tiket</a>
         <div class="menu-category">Akun</div>
